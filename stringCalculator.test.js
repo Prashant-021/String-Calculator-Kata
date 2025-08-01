d6cc601dafd479fb197e20a7293fb41e97b07d70
Prashant-iViewLabs <pspatel021@gmail.com>
2025-08-01 16:00:11 +0530

    test: add test case for multicharacter custom delimiter

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -60,7 +60,7 @@ describe('string with custom delimiter', () => {
 })
 
 describe('string with both custom and default delimiters', () => {
-    const testCases = [['//#\n1#2,4', 7], ['//#\n1#2,3\n4', 10],['//*\n1*2,3\n4', 10] ]
+    const testCases = [['//#\n1#2,4', 7], ['//#\n1#2,3\n4', 10], ['//*\n1*2,3\n4', 10]]
 
     testCases.forEach(([input, expected]) => {
         test(generateTestCaseTitle(input, expected), () => {
@@ -68,4 +68,11 @@ describe('string with both custom and default delimiters', () => {
         })
     })
 })
-
+describe('string with multicharacter custom delimiter', () => {
+    const testCases = [['//***\n1***2***3', 6],['//*#*\n1*#*2*#*3', 6]]
+    testCases.forEach(([input, expected]) => {
+        test(generateTestCaseTitle(input, expected), () => {
+            expect(stringCalculator(input)).toBe(expected)
+        })
+    })
+})
